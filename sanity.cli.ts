b872585a9ea90d45489457625f6d3b01d0f46de6
NinaHahne <ninamariehahne@gmail.com>
2026-03-22 00:15:23 +0100

    chore: add appId to sanity.cli.ts to avoid prompting for application id on next deploy

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'jhfft3cv',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'avjyezawsnxr0d5je41thnc1',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+  },
 })
